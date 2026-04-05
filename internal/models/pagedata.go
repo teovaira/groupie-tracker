f19ac771e621fd47028a7eb8449f8ad02a1a31fd
tvairakt <t.vairaktaris@gmail.com>
2026-04-05 08:17:39 +0300

    refactor(models): remove inline field comments from ArtistPageData

--- internal/models/pagedata.go
+++ internal/models/pagedata.go
@@ -6,7 +6,7 @@ package models
 // each concert location to the dates the artist performed there.
 type ArtistPageData struct {
 	Artist         Artist
-	Locations      []string            // flat list from /locations endpoint
-	Dates          []string            // flat list from /dates endpoint
-	DatesLocations map[string][]string // location→dates map from /relation endpoint; used by the template
+	Locations      []string
+	Dates          []string
+	DatesLocations map[string][]string
 }
