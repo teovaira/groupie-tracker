f54a4e64ec13a75e8357ed2ddc2e9f54e430b983
tvairakt <t.vairaktaris@gmail.com>
2026-04-13 09:16:48 +0300

    fix(cmd): comment out panic-test debug route

--- cmd/main.go
+++ cmd/main.go
@@ -72,9 +72,9 @@ func main() {
 	mux.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
 	mux.HandleFunc("GET /api/search", searchHandler.Search)
 
-	mux.HandleFunc("GET /panic-test", func(w http.ResponseWriter, r *http.Request) {
-    panic("intentional test panic")
-})
+	// mux.HandleFunc("GET /panic-test", func(w http.ResponseWriter, r *http.Request) {
+	// 	panic("intentional test panic")
+	// })
 
 	log.Printf("server listening on http://localhost%s", addr)
 	if err := http.ListenAndServe(addr, handlers.RecoveryMiddleware(serverErrorTmpl, mux)); err != nil && !errors.Is(err, http.ErrServerClosed) {
